ab554056968d2081e485a92952cfc503a50bb576
Rasmus Elmgren <rasmus@elmgren.net>
2023-08-21 08:09:58 +0200

    add a deploy script

---
+++ scripts/deploy.py
@@ -0,0 +1,17 @@
+import os
+
+def build():
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    docker_dir = os.path.join(current_dir, "..", "docker")
+    os.chdir(docker_dir)
+    os.system("docker-compose build")
+
+def push_to_dockerhub():
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    docker_dir = os.path.join(current_dir, "..", "docker")
+    os.chdir(docker_dir)
+    os.system("docker-compose push")
+
+if __name__ == "__main__":
+    build()
+    push_to_dockerhub()
